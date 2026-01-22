16856ef1f85be91ccf3213ce344e909c6b4bcedb
smiley texta <smileytexta314@gmail.com>
2026-01-22 17:52:25 +1100

    Created feedback table, experiment apparatus, updated choose_best_entropy

--- create_feedback_table.py
+++ create_feedback_table.py
@@ -1,1 +1,3 @@
-
+version https://git-lfs.github.com/spec/v1
+oid sha256:285788f297acea6ccce8c2596b2887e0736d38273a5ef4181b46165650c35f3f
+size 1978
--- experiments.py
+++ experiments.py
@@ -1,1 +1,3 @@
-
+version https://git-lfs.github.com/spec/v1
+oid sha256:4c3e5752acdb3f7cae5d2bc66e0ef34547314fc52c87aae88088bd1aff705bc0
+size 2951
