41f0586a73320a9d8f659334a7dbc3123dca8ed0
smiley texta <smileytexta314@gmail.com>
2026-01-30 19:37:58 +1100

    Test saving plots with matplotlib

--- experiments.py
+++ experiments.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7cf9742a8f0eeafdb10f235ab18c5f1c966c8e9396f68b35a899100143b616da
-size 3752
+oid sha256:d1ed11bdafb55d06144aa8ee7b68e0ef58c6640d489f437d737f12d1e9b88224
+size 3836
