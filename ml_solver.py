0f56186777b9052814181d63703358efcb9aa48a
smiley texta <smileytexta314@gmail.com>
2026-02-08 21:33:23 +1100

    Implement streamlit application

--- ml_solver.py
+++ ml_solver.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b09217870ad7b09772a06b514aa0987261bf21b38abadf61774f361444d4dd02
-size 6407
+oid sha256:1f97195b845d8bb5216fe3f5b273e6f62ddba5ec22402dbf8e8f140b7357b3ea
+size 6451
---
+++ streamlit_app.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a96abb5af03e98bf2f60644ddd0f0ebc1b238f08195175f63c1372692fc13460
+size 7723
